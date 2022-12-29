d70e53116799aeeddbe93bef954f90ee697ecf17
prawsopit <prawsopit@gmail.com>
2022-12-29 13:07:10 +0700

    8.3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,12 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x)
+{
+	char y;
+	if (x > 65 && x <= 90)
+	{
+		y = x - 1;
+		return y;
+	}
+	else if (x == 65)
+	{
+		return 'Z';
+	}
+	return '0';
 }
 
-int main(){
-	//Test Case
+int main()
+{
+	// Test Case
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
