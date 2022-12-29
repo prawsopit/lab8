d1da3de6152022aa752c887322b31cf886b864c0
prawsopit <prawsopit@gmail.com>
2022-12-29 12:29:05 +0700

    finish

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
+    int a  =  20 , b = 30;
+    int c = 4 ;
+    float output ,d ;
+    
+ d=(float)b/c ;
+    output = (float)a - d;
+    cout << "output: " << output  ;
     return 0;
 }
